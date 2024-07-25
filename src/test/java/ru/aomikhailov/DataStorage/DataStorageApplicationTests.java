bab964916b35a5afde4fada5dea33cfaad361318
funtik22 <mega.artemm@bk.ru>
2024-07-25 09:30:45 +0300

    initial commit

---
+++ src/test/java/ru/aomikhailov/DataStorage/DataStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.aomikhailov.DataStorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataStorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
